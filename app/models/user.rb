bebd44462761e039e534a2d52316f26d72bc691e
karuna24s <karuna24s@gmail.com>
2016-09-23 23:15:57 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   def create_current_cart
     new_cart = carts.create
-    self.current_cart = new_cart.id
+    self.current_cart_id = new_cart.id
     save
   end
 
